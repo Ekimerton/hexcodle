6a52eb4654a8722eb78091dc8f0d6e5039685e1e
Hannah Larsen <18hkl@queensu.ca>
2023-07-04 20:06:00 -0400

    Updated display text and added link to html picker

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,9 @@ export default function Home() {
   const [userInput, setUserInput] = useState("#");
   const [randColor, setRandColor] = useState("bisque");
   const [statusText, setStatusText] = useState("");
+  const [guessText, setGuessText] = useState("");
   const [guesses, setGuesses] = useState([]);
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(4);
 
   useEffect(() => {
     const r = Math.floor(Math.random() * 256);
@@ -44,20 +45,27 @@ export default function Home() {
       - has it not been previously guessed */
     }
     if (userInput.length != 7) {
-      setStatusText("ERROR: HEX CODE MUST BE EXACTLY 6 DIGITS");
+      setStatusText("ERROR: HEX CODE MUST BE EXACTLY 6 DIGITS.");
       return;
+    } else if (userInput in guesses) {
+      setStatusText("ALREADY GUESSED, please try a different hex code.");
     } else {
-      setCounter(counter + 1);
-      if (counter < 4 && userInput === randColor) {
+      setCounter(counter - 1);
+      if (counter > 0 && userInput === randColor) {
         setStatusText("You guessed it!");
         return;
-      } else if (counter < 4 && userInput !== randColor) {
-        setStatusText("Not quite!", counter, " guesses left.");
+      } else if (counter > 0 && userInput !== randColor) {
+        setGuessText("Guesses: ");
+        setStatusText(`Not quite! ${counter} guesses left.`);
         const newGuesses = [...guesses];
         newGuesses.unshift(userInput);
+        {
+          /*add line break here for each guess on newline*/
+        }
         setGuesses(newGuesses);
-      } else if (counter >= 4 && userInput !== randColor) {
-        setStatusText("Out of guesses. Today's Hexcodle was ", randColor);
+      } else if (counter <= 0 && userInput !== randColor) {
+        setGuessText("Guesses: ");
+        setStatusText(`Out of guesses. Today's Hexcodle was ${randColor}.`);
         return;
       }
     }
@@ -67,14 +75,23 @@ export default function Home() {
     <div id="everything">
       <h1 id="title">Hexcodle</h1>
 
+      {/*Instructions for how game works and hyperlink to colour code picker site*/}
+      <p id="instructions">
+        You will have 5 tries to correctly guess the hex code of the colour
+        displayed on screen.<br></br>
+        Don't know hex codes byb memory? No worries! Try the{" "}
+        <a href="https://htmlcolorcodes.com/color-picker/">
+          html colour picker
+        </a>{" "}
+        site!
+      </p>
+
       {/*This is where the hex code colour will be generated*/}
       <div id="colourGen" style={{ backgroundColor: randColor }}></div>
-
       <p>{randColor}</p>
 
-      {/*<p id="instructions">The goal: guess the hex code of the colour displayed on screen.<br></br>
-      Don't know hex codes? No worries! We have a hex code cheat sheet! Just simply click the 'hex' button!</p>*/}
-
+      {/*Input text field and 'enter' button for submitting an answer 
+      NEED TO IMPLEMENT PRESSING ENTER TO SUBMIT AS WELL*/}
       <div id="inputAndButton">
         <input
           type="text"
@@ -82,6 +99,8 @@ export default function Home() {
           value={userInput}
           onChange={handleChange}
         />
+
+        {/*Enter button that will submit player's guess when pressed*/}
         <button
           id="enterButton"
           onClick={() => {
@@ -91,8 +110,13 @@ export default function Home() {
           Enter
         </button>
       </div>
+
+      {/*Displays the status text (correct, incorrect, error, etc...*/}
       <p>{statusText}</p>
-      <p>{guesses}</p>
+      <p id="guessDisplay">
+        {guessText}
+        {guesses}
+      </p>
     </div>
   );
 }
