3fb1a772f2c2d97ffe4185c9f29ea34f02724e25
Ekim <ekim0252@gmail.com>
2023-08-11 05:15:59 +0000

    Swag utils

---
+++ src/app/utils.js
@@ -0,0 +1,74 @@
+// For Hansies :^)
+import { useState } from "react";
+
+/*
+    Function to generate a random number based on the current date
+    This is a pseudo-random function that generated a number between (0 and n].
+    Normally you'd have to generate a new target every day and store that in a database
+    but this makes it so everyone has the same random target every day,
+    while needing no server :>
+*/ 
+export function generateUniqueNumber(n) {
+    const now = moment().tz("America/New_York");
+    const seed = now.format("DD-MM-YYYY");
+    const rng = seedrandom(seed);
+    const uniqueNumber = Math.floor(rng() * (Number(n) + 1));
+    return uniqueNumber;
+}
+
+/*
+    This is a basic hook for using the useState hook in react, but with
+    local storage! So instead of doing
+
+    const [guess, setGuess] = useState("")
+    
+    You can do:
+
+    const [guess, setGuess] = useLocalStorage("previousGuesses", "");
+
+    Like normal useState it takes an initial value as one of the params (2nd param),
+    and the first param is a key which creates a store for it in the browser storage.
+*/
+export default function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+}
+
+// Function for converting 0-15 to a character
+function decimalToHex(n) {
+    if (n < 0 || n > 15) {
+        throw new Error('Input must be a number between 0 and 15.');
+    }
+
+    return n.toString(16);
+}
+
+// Function for converting a character to 0-15
+function hexToDecimal(hexChar) {
+    if (typeof hexChar !== 'string' || !hexChar.match(/^[0-9a-fA-F]$/)) {
+        throw new Error('Input must be a char between 0 and f.');
+    }
+
+    return parseInt(hexChar, 16);
+}
+
